960a0e91d1276182ed2ca32941e432c2116aad3f
azimut <nicolas.marguglio@gmail.com>
2014-04-04 00:20:15 -0300

    chall 1

---
+++ scripts/1_randstring.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Description: scramble the given string
+
+# $ ./randstring.sh abcdefghijklmnopqrstuvwxyz
+#  String original:    abcdefghijklmnopqrstuvwxyz
+#  String desordenada: bljcqyrmanpfizsdhugxwtkove
+
+[[ -z $1 ]] && {
+    echo 'Missing argument'
+    exit 1
+}
+
+echo    'String original:    '"$1"
+echo -n 'String desordenada: '
+echo -n "${1}" | while read -n1 -r letter; do 
+     echo $letter
+done | sort -R | xargs -ILETTER echo -n LETTER
+echo
