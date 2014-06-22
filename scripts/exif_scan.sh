91772cb331e47ae8efd8c3fa51d515dab7b0cc4c
azimut <nicolas.marguglio@gmail.com>
2014-06-22 19:29:03 -0300

    new inst files

---
+++ scripts/exif_scan.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# Description: extract exif data from images using different tools
+# Usage: accept one argument or a list of arguments
+# ./exif_scan.sh image.jpg
+# ./exif_scan.sh *.jpg
+echoname(){
+  tput setf 2
+  echo '=====> '"$@"
+  tput sgr0
+}
+
+exif.get(){
+  {
+    set -x
+    exif "$@"
+    exif --extract-thumbnail -- "$@"
+    exiftool "$@"
+    exiftags -a "$@"
+    { set +x; } &> /dev/null
+  } 2>&1
+}
+
+for img in "$@"; do
+  if [[ -f $img ]]; then
+    echoname "$img"
+    exif.get "$img"
+  fi
+done
