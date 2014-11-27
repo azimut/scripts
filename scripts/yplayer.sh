71965c1586b515e14d85815ecb51448c40837af2
azimut <nicolas.marguglio@gmail.com>
2014-11-26 21:47:35 -0300

    yt script

---
+++ scripts/yplayer.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+TEMP_FILE=$(mktemp)
+YOUTUBE_DL_OPTIONS=(-f 18)
+
+ctrl_c(){
+  set -x
+  rm -f $TEMP_FILE
+  kill $queue_pid
+  exit 1
+}
+
+trap ctrl_c SIGINT
+
+echoerr()  { echo -en '\x1b[31;01m[-]\x1b[39;49;00m ' 1>&2; echo "$@" 1>&2; }
+
+check_in_path(){
+  local cmd=$1
+  hash "$cmd" &>/dev/null || {
+    echoerr "uError: command \"$cmd\" not found in \$PATH, please add it or install it..."
+    exit 1
+  }
+}
+
+check_in_path 'mplayer'
+check_in_path 'youtube-dl'
+
+# Description: launch a mplayer process
+# INPUT: normal url of a video
+# OUTPUT: none
+yplayer(){
+  local yurl=$1
+  local regex_y_video='^http://.*'
+  local y_video
+
+#  set -x
+  until [[ ! -z $y_video && $y_video =~ $regex_y_video ]]; do
+    y_video=$(youtube-dl --prefer-insecure -g "${yurl}")
+  done
+
+  until [[ ! -z "$(pgrep mplayer)" || ! -z $(pgrep youtube-dl) ]]; do
+    mplayer -vo gl  -vfm ffmpeg -lavdopts lowres=0:fast:skiploopfilter=all \
+            -cache 8146 \
+            -cache-min 20 \
+            -prefer-ipv4 \
+            -framedrop \
+            "${y_video}" </dev/null >/dev/null 2>&1 &
+  
+    disown
+    sleep 10 # idle time to make sure the mplayer process didn't died
+#    set +x
+  done
+}
+
+# Description: if no "mplayer" process is found, send the url to the function in charge of spawn mplayer
+# INPUT: none / pgrep mplayer
+# OUTPUT: none
+queue(){
+  while :; do
+    if [[ -z "$(pgrep mplayer)" &&  -s $TEMP_FILE ]]; then
+      yplayer "$(head -n1 $TEMP_FILE)"
+      sed -i '1d' $TEMP_FILE
+    fi
+    sleep 5 # <--- idle time between each queue check
+  done
+}
+
+# fork the function that watch the queue
+queue &
+queue_pid=$! # obtain the PID of the queue
+
+# Description: "UI" that waits for user input
+# INPUT: keyboard
+# OUTPUT: temporary file
+while :; do
+  read -r -p 'Provide a valid youtube URL: ' yurl
+  echo "${yurl}" | cut -f1 -d'&' >> $TEMP_FILE
+done
