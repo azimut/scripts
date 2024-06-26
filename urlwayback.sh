29ab2dafb3f32e969c758591fa7712a694f43ff8
azimut <azimut.github@protonmail.com>
2024-06-25 23:10:59 -0300

    urlwayback.sh: add flag for getting latest url

--- urlwayback.sh
+++ urlwayback.sh
@@ -1,9 +1,16 @@
 #!/bin/sh -eu
 
 # Description: Returns the oldest url available
-# Documentation: https://archive.org/help/wayback_api.php
+# See: https://archive.org/help/wayback_api.php
 
-[ $# -ne 1 ] && echo "Error: needs an url parameter." && exit 1
+err() { echo "[ERROR]: $*" >&2; }
+usage() {
+	echo "Usage:"
+	printf '\t%s URL [ NEW? ]\n' "$(basename $0)"
+}
 
-curl -s "http://archive.org/wayback/available?url=${1%#*}&timestamp=19960101" |
+[ $# -lt 1 ] && err "missing URL argument" && usage && exit 1
+[ $# -gt 1 ] && TIMESTAMP='' || TIMESTAMP='&timestamp=19960101'
+
+curl -s "http://archive.org/wayback/available?url=${1%#*}${TIMESTAMP}" |
 	jq -r '.archived_snapshots.closest | .status + " " + .url'
