7d766e1c4a53361b4acd795cc085b161c251a504
azimut <azimut.github@protonmail.com>
2024-06-28 09:57:32 -0300

    ytradio.sh: return proper exit code when arguments are invalid

--- ytradio.sh
+++ ytradio.sh
@@ -9,8 +9,8 @@ usage() {
 	printf "\t%s [URLS...]\n" "$(basename $0)"
 }
 
-(($# != 1)) && err "no url provided" && usage && exit 1
-[[ $1 != *playlist* ]] && err "invalid url provided" && usage && exit
+(($# != 1)) && err "no url provided" && usage && exit 22
+[[ $1 != *playlist* ]] && err "invalid url provided" && usage && exit 22
 
 mpv --ytdl-format=bestaudio \
 	--ytdl-raw-options='playlist-random=' \
