065ed839ea7a5b5955f3be67190c871653e54ca4
azimut <azimut.github@protonmail.com>
2024-06-28 10:30:50 -0300

    ytradio.sh: add a "memory" of played playlists
    avoid error prone maintenance of the file

--- ytradio.sh
+++ ytradio.sh
@@ -12,6 +12,15 @@ usage() {
 (($# != 1)) && err "no url provided" && usage && exit 22
 [[ $1 != *playlist* ]] && err "invalid url provided" && usage && exit 22
 
-mpv --ytdl-format=bestaudio \
-	--ytdl-raw-options='playlist-random=' \
-	"$1"
+remember() {
+	local queue="${HOME}/Music/music.txt"
+	grep -xFq "$1" "${queue}" || echo "$1" >>"${queue}"
+}
+play() {
+	mpv --ytdl-format=bestaudio \
+		--ytdl-raw-options='playlist-random=' \
+		"$1"
+}
+
+play "$1"
+remember "$1" # here we should know for sure it's a good link
