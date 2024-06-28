f7329f3f7d15b5d2bc3a0b3942daf74658dae5c5
azimut <azimut.github@protonmail.com>
2024-06-28 11:52:32 -0300

    urlwayback.sh: add error handling

--- urlwayback.sh
+++ urlwayback.sh
@@ -11,8 +11,21 @@ usage() {
 	printf '\t%s URL [ NEW? ]\n' "$(basename $0)"
 }
 
-[ $# -lt 1 ] && err "missing URL argument" && usage && exit 1
+[ $# -lt 1 ] && err "missing URL argument" && usage && exit 22
 [ $# -gt 1 ] && TIMESTAMP='' || TIMESTAMP='&timestamp=19960101'
 
 curl -s "http://archive.org/wayback/available?url=${1%#*}${TIMESTAMP}" |
-	jq -r '.archived_snapshots.closest | .status + " " + .url'
+	jq -r '.archived_snapshots.closest | .status + " " + .url' |
+	while read -r status url; do
+		case "${status}" in
+		200) echo "${url}" ;;
+		"")
+			echo "ERROR: no snapshot found for url :(" >&2
+			exit 61
+			;;
+		*)
+			echo "WARNING: found snapshot has http code ${status}" >&2
+			echo "${url}"
+			;;
+		esac
+	done
