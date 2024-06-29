552a4ae990d53ebdd70779c4add417c04eead5bb
azimut <azimut.github@protonmail.com>
2024-06-29 16:47:29 -0300

    paths.rb: refactor

--- paths.rb
+++ paths.rb
@@ -2,8 +2,8 @@
 
 missings = ENV['PATH'].split(':').reject { File.directory? _1 }.sort
 
-if missings.count.zero?
-  puts "All directories on $PATH exist. Good job!"
+if missings.empty?
+  puts 'All directories on $PATH exist. Good job!'
 else
   puts missings
   puts "\nFound #{missings.count} non-existent dirs on $PATH!"
