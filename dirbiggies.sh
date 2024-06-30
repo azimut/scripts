381acb69128d40949ef2b9306fee6648400ff256
azimut <azimut.github@protonmail.com>
2024-06-30 01:18:54 -0300

    dirbiggies.sh: init

---
+++ dirbiggies.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -euo pipefail
+
+find . -type f -printf '%s %p\n' |
+	sort -rn |
+	head -n ${1:-10} |
+	while read -r size file; do
+		printf '%5s\t%s\n' "$(numfmt --to=iec ${size})" "${file}"
+	done
