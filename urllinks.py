eb622deceebaa46f107366cfb8b052524eab70e8
azimut <azimut.github@protonmail.com>
2024-07-08 08:17:15 -0300

    urllinks.py: resolve relative links

--- urllinks.py
+++ urllinks.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 
 import sys
-from os.path import basename
+import os.path
 from bs4 import BeautifulSoup
 from urllib.parse import urlparse, urldefrag
 from urllib.request import urlopen
@@ -9,19 +9,25 @@ from urllib.request import urlopen
 def usage():
     print("List external links in an url.")
     print("Usage:")
-    print(f"\t{basename(sys.argv[0])} URL")
+    print(f"\t{os.path.basename(sys.argv[0])} URL")
 
 
 def main():
-    html = urlopen(sys.argv[1]).read()
+    url = sys.argv[1]
+    html = urlopen(url).read()
     soup = BeautifulSoup(html, 'html.parser')
     ulinks = set()
+    dirname = os.path.dirname(url)
 
     for a in soup.find_all('a'):
         href = a.get('href')
-        if href and urlparse(href).scheme:
-            url, _ = urldefrag(href)
-            ulinks.add(url)
+        if not href:
+            continue
+        if urlparse(href).scheme:
+            link, _ = urldefrag(href)
+            ulinks.add(link)
+        else:
+            ulinks.add(dirname + "/" + href)
 
     for link in sorted(ulinks,key=lambda x: urlparse(x).netloc):
         print(link)
