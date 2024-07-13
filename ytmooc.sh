bff33e647c39754974a0947bd5928e40ce733835
azimut <azimut.github@protonmail.com>
2024-07-13 07:46:57 -0300

    ytmooc.sh: add low audio quality knob

--- ytmooc.sh
+++ ytmooc.sh
@@ -6,18 +6,20 @@ err() { echo "ERROR: $*" >&2; }
 usage() {
 	echo -e "yt-dlp flag wrapper to download playlists of videos"
 	echo "Usage:"
-	echo -e "\t$(basename $0) [-S] [-I] [-r RATE] [-s START] [-m MAXHEIGHT] URL"
+	echo -e "\t$(basename $0) [-S] [-I] [-A] [-r RATE] [-s START] [-v MAXHEIGHT] URL"
 }
 
 OPTS=()
-while getopts ":hSIm:s:r:" arg; do
+OPTS=(--format='bestvideo+bestaudio')
+while getopts ":hSIAv:s:r:" arg; do
 	case $arg in
 	h) echo "help" ;;
 	S) OPTS+=(--write-subs --sub-langs 'en.*,es.*') ;;
 	I) OPTS+=(--output='%(playlist_index)03d-%(title)s[%(id)s].%(ext)s') ;;
+	A) OPTS=(${OPTS/bestaudio/bestaudio[asr<40k]}) ;;
+	v) OPTS=(${OPTS/bestvideo/bestvideo[height<$OPTARG]}) ;;
 	r) OPTS+=(--limit-rate="${OPTARG}") ;;
 	s) OPTS+=(--playlist-start="${OPTARG}") ;;
-	m) OPTS+=(--format='bestvideo[height<'"${OPTARG}"']+bestaudio') ;;
 	:)
 		err "Mandatory argument missing for given flag $OPTARG"
 		usage
