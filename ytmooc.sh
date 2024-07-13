50d4da09f0c53dd00bef1505f5bcfaca2b32b6d1
azimut <azimut.github@protonmail.com>
2024-07-13 07:23:04 -0300

    ytmooc.sh: add getopts flags

--- ytmooc.sh
+++ ytmooc.sh
@@ -1,23 +1,40 @@
 #!/bin/bash
 
-# Description: Downloads a playlist of a MOOC
-# - With subs
-# - With low video quality for space
-# - With the best audio (TODO: converted to mono)
-# - Name numbered
+set -eu
 
-set -exuo pipefail
+err() { echo "ERROR: $*" >&2; }
+usage() {
+	echo -e "yt-dlp flag wrapper to download playlists of videos"
+	echo "Usage:"
+	echo -e "\t$(basename $0) [-S] [-I] [-r RATE] [-s START] [-m MAXHEIGHT] URL"
+}
 
+OPTS=()
+while getopts ":hSIm:s:r:" arg; do
+	case $arg in
+	h) echo "help" ;;
+	S) OPTS+=(--write-subs --sub-langs 'en.*,es.*') ;;
+	I) OPTS+=(--output='%(playlist_index)03d-%(title)s[%(id)s].%(ext)s') ;;
+	r) OPTS+=(--limit-rate="${OPTARG}") ;;
+	s) OPTS+=(--playlist-start="${OPTARG}") ;;
+	m) OPTS+=(--format='bestvideo[height<'"${OPTARG}"']+bestaudio') ;;
+	:)
+		err "Mandatory argument missing for given flag $OPTARG"
+		usage
+		exit 1
+		;;
+	\?)
+		err "Unknown flag"
+		usage
+		exit 1
+		;;
+	esac
+done
+shift $((OPTIND - 1)) # allow positional arguments
+
+(($# != 1)) && err "Missing URL parameter." && usage && exit 1
+
+set -x
 URL="${1}"
-LESS="${2:-720}"
-START="${3-1}"
-
 echo -n "${URL}" >url
-
-yt-dlp \
-	--playlist-start="${START}" \
-	--output '%(playlist_index)03d-%(title)s[%(id)s].%(ext)s' \
-	--format 'bestvideo[height<'"${LESS}"']+bestaudio' \
-	--write-subs --sub-langs 'en.*,es.*' \
-	--continue \
-	"${URL}"
+yt-dlp "${OPTS[@]}" --continue "${URL}"
