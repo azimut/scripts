ca46dec9354d791843ba37fe1e4290e7d8808d08
azimut <azimut.github@protonmail.com>
2024-08-03 09:18:07 -0300

    urlwayback.sh: handle proto on url

--- urlwayback.sh
+++ urlwayback.sh
@@ -14,7 +14,10 @@ usage() {
 [ $# -lt 1 ] && err "missing URL argument" && usage && exit 22
 [ $# -gt 1 ] && TIMESTAMP='' || TIMESTAMP='&timestamp=19960101'
 
-curl -s "http://archive.org/wayback/available?url=${1%#*}${TIMESTAMP}" |
+URL=${1%#*}     # remove url fragments
+URL=${URL#*://} # remove url proto
+
+curl -s "http://archive.org/wayback/available?url=${URL}${TIMESTAMP}" |
 	jq -r '.archived_snapshots.closest | .status + " " + .url' |
 	while read -r status url; do
 		case "${status}" in
