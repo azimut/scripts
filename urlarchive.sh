1773571b20964db6d882495c61da4cdaa8bb4579
azimut <azimut.github@protonmail.com>
2024-08-11 02:11:52 -0300

    urlarchive.sh: init mvp

---
+++ urlarchive.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+set -eu
+
+err() { echo "ERROR:" >&2; }
+usage() {
+	cat <<EOF
+Tries to get the snapshot from archive.is.
+Usage:
+    $(basename $0) URL
+EOF
+}
+
+[ $# -ne 1 ] && usage && exit 1
+
+BASE='https://archive.is/timegate'
+URL="${1%#*}" # remove url fragments
+
+curl -Ls -o /dev/null -w '%{http_code} %{url_effective}' "${BASE}/${URL}" |
+	while read -r code effectiveurl; do
+		case "${code}" in
+		404)
+			err "No archived :("
+			exit 1
+			;;
+		*)
+			echo "STATUS: ${code}"
+			echo "URL: ${effectiveurl}"
+			;;
+		esac
+	done
