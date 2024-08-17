ed40f794aa5e87c82704e79368b2c6eb654b569d
azimut <azimut.github@protonmail.com>
2024-08-17 07:39:14 -0300

    ffmpegsumtime.sh: force gawk since is by default unbuffered

--- ffmpegsumtime.sh
+++ ffmpegsumtime.sh
@@ -4,4 +4,4 @@
 find . -regextype egrep -iregex '.*(mp4|mp3|webm)' -print0 |
 	xargs -n1 -0 -r -P2 \
 		ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 |
-	awk '{ printf("\033[10D%s", strftime("%H:%M:%S", t+=$1, 1)); } END { if(t) print("") }'
+	gawk '{ printf("\033[10D%s", strftime("%H:%M:%S", t+=$1, 1)); } END { if(t) print("") }'
