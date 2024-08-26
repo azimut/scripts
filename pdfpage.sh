62b4022ad13f7eb876f0d830583d9954011d9fd0
azimut <azimut.github@protonmail.com>
2024-08-26 03:56:00 -0300

    pdfpage.sh: init

---
+++ pdfpage.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -eu
+
+err() { echo "ERROR: $*" >&2; }
+usage() {
+	cat <<EOF
+Saves pdf PAGE as a png image.
+Usage:
+    $(basename $0) <PDF> <PAGE>
+EOF
+}
+
+FILE="$1"
+PAGE="$2"
+
+[ $# -ne 2 ] && usage && exit 1
+[ ! -s "${FILE}" ] && err "Given file does NOT exist." && exit 1
+[[ ${FILE,,} != *pdf ]] && err "Given file is not a .pdf?" && exit 1
+
+OUTPUT="${FILE}.${PAGE}.png"
+
+pdftoppm "${FILE}" "${OUTPUT}" -png -f "${PAGE}" -singlefile
