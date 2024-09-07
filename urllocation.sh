9f5f535823f24ef9799df68072c91d5fe0281d26
azimut <azimut.github@protonmail.com>
2024-09-06 23:49:21 -0300

    urllocation.sh: mvp

---
+++ urllocation.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -euo pipefail
+
+usage() {
+	cat <<EOF
+Returns each url Location jump for the given URL.
+
+Usage:
+    $(basename $0) <URL>
+EOF
+}
+
+(($# != 1)) && usage && exit 1
+
+URL="$1"
+echo "00: ${URL}"
+
+i=0
+torsocks curl -svo /dev/null -L "${URL}" 2>&1 | grep -i 'location:' |
+	while read -r _ _ location; do
+		printf '%02d: %s\n' "$((++i))" "${location}"
+	done
