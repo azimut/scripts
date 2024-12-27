a87c0f760c5bfc6e2136f2f853787ba0e62e0476
azimut <azimut.github@protonmail.com>
2024-12-27 05:29:21 -0300

    ffmpegsumtime: format + change escape sequence

--- ffmpegsumtime.sh
+++ ffmpegsumtime.sh
@@ -2,6 +2,6 @@
 
 # Sums up the reproduction time of all media under CWD
 find . -regextype egrep -iregex '.*(mp4|mp3|webm)' -print0 |
-	xargs -n1 -0 -r -P2 \
-		ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 |
-	gawk '{ printf("\033[10D%s", strftime("%H:%M:%S", t+=$1, 1)); } END { if(t) print("") }'
+    xargs -n1 -0 -r -P2 \
+        ffprobe -v error -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 |
+    gawk '{ printf("\033[E%s", strftime("%H:%M:%S", t+=$1, 1)); } END { if(t) print("") }'
