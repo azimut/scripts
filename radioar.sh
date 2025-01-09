4ac8bd78aac221924f9963cb4311549cc54ea7cb
azimut <azimut.github@protonmail.com>
2025-01-08 21:16:16 -0300

    radioar.sh: add bkt cache + while infinity loop

--- radioar.sh
+++ radioar.sh
@@ -2,9 +2,11 @@
 set -eo pipefail
 export LC_LOCALE=C
 
-curl 'http://all.api.radio-browser.info/json/stations/bycountrycodeexact/ar' |
-    jq -r 'unique_by(.url_resolved)[] | [.name,.url_resolved] | @csv' |
-    sort -k1,1 -t, |
-    fzf --with-nth=1 --delimiter=, |
-    cut -f2 -d, |
-    xargs --no-run-if-empty mpv
+while :; do
+    bkt --ttl=60m -- curl 'http://all.api.radio-browser.info/json/stations/bycountrycodeexact/ar' |
+        jq -r 'unique_by(.url_resolved)[] | [.name,.url_resolved] | @csv' |
+        sort -k1,1 -t, |
+        fzf --with-nth=1 --delimiter=, |
+        cut -f2 -d, |
+        xargs --no-run-if-empty mpv
+done
