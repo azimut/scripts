84db97cd52acdbbfd22d965a26298d0391582ad1
azimut <azimut.github@protonmail.com>
2025-01-08 21:02:09 -0300

    radioar.sh: use curl

--- radioar.sh
+++ radioar.sh
@@ -2,8 +2,7 @@
 set -eo pipefail
 export LC_LOCALE=C
 
-# curl http://all.api.radio-browser.info/json/stations/bycountrycodeexact/ar
-cat "${HOME}/projects/elm/radios/radios.ar.json" |
+curl 'http://all.api.radio-browser.info/json/stations/bycountrycodeexact/ar' |
     jq -r 'unique_by(.url_resolved)[] | [.name,.url_resolved] | @csv' |
     sort -k1,1 -t, |
     fzf --with-nth=1 --delimiter=, |
