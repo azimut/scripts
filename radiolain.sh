299a1c828dc6128c6d9c30942575ba7ee6489b0f
azimut <azimut.github@protonmail.com>
2025-01-08 21:30:17 -0300

    radiolain.sh: bkt 24 hours, enable all pages

--- radiolain.sh
+++ radiolain.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-
 set -eo pipefail
+BASEURL='https://lainchan.org/music/'
+PAGES=('' '2.html' '3.html' '4.html' '5.html')
 
-readonly BASEURL='https://lainchan.org/music/'
-readonly PAGES=('' '2.html') # '3.html' '4.html' '5.html'
-
-lynx -dump -listonly -nonumbers "${PAGES[@]/#/${BASEURL}}" |
+bkt --ttl=24hour -- lynx -dump -listonly -nonumbers "${PAGES[@]/#/${BASEURL}}" |
     awk -W interactive \
         '/(mp3|ogg)$/ && !seen[$0]++'
