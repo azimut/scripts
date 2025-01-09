1cd019ff637eaf19ab8fe6f22ffe82800f33167d
azimut <azimut.github@protonmail.com>
2025-01-09 15:35:49 -0300

    radioar.sh: remove dups with jq

--- radioar.sh
+++ radioar.sh
@@ -3,9 +3,13 @@ set -eo pipefail
 export LC_LOCALE=C
 
 while :; do
-    bkt --ttl=60m -- curl 'http://all.api.radio-browser.info/json/stations/bycountrycodeexact/ar' |
-        jq -r 'unique_by(.url_resolved)[] | [.name,.url_resolved] | @csv' |
-        sort -k1,1 -t, |
+    bkt --ttl=24hour -- curl 'http://all.api.radio-browser.info/json/stations/bycountrycodeexact/ar' |
+        jq -r 'def trim: gsub("^[ ]+|[ ]+$";"");
+               unique_by(.url_resolved) |
+               unique_by(.name|ascii_downcase|trim)[] |
+               [(.name|trim),.url_resolved] |
+               @csv' |
+        sort |
         fzf --with-nth=1 --delimiter=, |
         cut -f2 -d, |
         xargs --no-run-if-empty mpv
