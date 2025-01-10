b34d82878a70d6e11dc1f24f7ddc4dcefa2dfdd1
azimut <azimut.github@protonmail.com>
2025-01-10 18:35:17 -0300

    radiolain.sh: add youtube links

--- radiolain.sh
+++ radiolain.sh
@@ -5,4 +5,4 @@ PAGES=('' '2.html' '3.html' '4.html' '5.html')
 
 bkt --ttl=24hour -- lynx -dump -listonly -nonumbers "${PAGES[@]/#/${BASEURL}}" |
     awk -W interactive \
-        '/(mp3|ogg)$/ && !seen[$0]++'
+        '(/(mp3|ogg)$/||/youtube.com\/watch/||/youtu\.be/) && !seen[$0]++'
