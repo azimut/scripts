c16a4edb10402cd5a6e0b50cd44d15d13ff716bd
azimut <azimut.github@protonmail.com>
2025-01-10 18:10:21 -0300

    radioar.sh: add filter for .lastcheckok

--- radioar.sh
+++ radioar.sh
@@ -5,6 +5,7 @@ export LC_LOCALE=C
 while :; do
     bkt --ttl=24hour -- curl -s 'http://all.api.radio-browser.info/json/stations/bycountrycodeexact/ar' |
         jq -r 'def trim: gsub("^[ ]+|[ ]+$";"");
+               map(select(.lastcheckok > 0)) |
                unique_by(.url_resolved) |
                unique_by(.name|ascii_downcase|trim)[] |
                [(.name|trim),.url_resolved] |
