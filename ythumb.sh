b3bef97d8c29da3557ae3e337b391ad83cd819f5
azimut <azimut.github@protonmail.com>
2025-01-16 09:51:07 -0300

    ythumb: add back usage help

--- ythumb.sh
+++ ythumb.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 set -eo pipefail
 
+function usage() {
+    cat <<EOF
+Returns the thumbnails urls of the given youtube video.
+
+    $(basename "$0") [URL]
+EOF
+}
+
+(($# != 1)) && usage && exit 1
+
 youtube-dl --list-thumbnails "$1" |
     awk 'int($2) >= 400{ printf "%9s %s\n", $2"x"$3, $4 }'
