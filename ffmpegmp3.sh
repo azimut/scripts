fc5b91a7fc4790b1e587c5e46c840a67556761da
azimut <azimut.github@protonmail.com>
2025-01-26 01:47:37 -0300

    ffmpegmp3: format

--- ffmpegmp3.sh
+++ ffmpegmp3.sh
@@ -9,7 +9,7 @@ div() { echo "$(</dev/stdin) / ${1}" | bc -l; }
 round() { awk '{ print(int($0)) }'; }
 timestamp() { date -u --date=@"${1}" +%H:%M:%S; }
 seconds() {
-	ffprobe -loglevel error -hide_banner -of csv=p=0 -show_entries format=duration "${1}"
+    ffprobe -loglevel error -hide_banner -of csv=p=0 -show_entries format=duration "${1}"
 }
 
 output="$(basename "${INPUT}").mp3"
@@ -18,18 +18,18 @@ step="$(seconds "${INPUT}" | div ${N} | round)"
 rm -f extract_tmp*jpg
 
 for ((i = 0; i < N; i++)); do
-	ffmpeg -hide_banner -loglevel error \
-		-ss "$(timestamp $((step * i)))" \
-		-i "${INPUT}" \
-		-frames:v 1 \
-		"extract_tmp${i}.jpg"
+    ffmpeg -hide_banner -loglevel error \
+        -ss "$(timestamp $((step * i)))" \
+        -i "${INPUT}" \
+        -frames:v 1 \
+        "extract_tmp${i}.jpg"
 done
 
 ffmpeg -hide_banner \
-	-i "${INPUT}" \
-	-i "$(ipickme -s 200 ./extract_tmp*jpg)" \
-	-y \
-	-ac 1 -c:v mjpeg -map 0:a -map 1:v \
-	"${output}"
+    -i "${INPUT}" \
+    -i "$(ipickme -s 200 ./extract_tmp*jpg)" \
+    -y \
+    -ac 1 -c:v mjpeg -map 0:a -map 1:v \
+    "${output}"
 
 rm -f extract_tmp*jpg
