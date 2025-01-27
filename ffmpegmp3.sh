273e9bf40e9af9214292741807dedfcc33cdcfbd
azimut <azimut.github@protonmail.com>
2025-01-27 02:08:15 -0300

    ffmpegmp3: refactor

--- ffmpegmp3.sh
+++ ffmpegmp3.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
 set -exuo pipefail
 
-N=5
+timestamp() { date -u --date=@"${1}" +%H:%M:%S; }
+seconds() { ffprobe -of csv=p=0 -show_entries format=duration "${1}" 2>/dev/null; }
+cleanup_thumbnails() { rm -f extract_tmp*jpg; }
+
+trap 'cleanup_thumbnails' EXIT
+
+N=5 # number of thumbnails
 INPUT="$1"
 test -f "${INPUT}"
 
-timestamp() { date -u --date=@"${1}" +%H:%M:%S; }
-seconds() {
-    ffprobe -loglevel error -hide_banner -of csv=p=0 -show_entries format=duration "${1}"
-}
-
-output="$(basename "${INPUT}").mp3"
+# Extract Thumbnails
 step="$(seconds "${INPUT}" | dc -e "?$N/p")"
-
-rm -f extract_tmp*jpg
-
 for ((i = 0; i < N; i++)); do
     ffmpeg -hide_banner -loglevel error \
         -ss "$(timestamp $((step * i)))" \
@@ -23,11 +21,9 @@ for ((i = 0; i < N; i++)); do
         "extract_tmp${i}.jpg"
 done
 
-ffmpeg -hide_banner \
+# Create .mp3
+ffmpeg -y -hide_banner \
     -i "${INPUT}" \
     -i "$(ipickme -s 200 ./extract_tmp*jpg)" \
-    -y \
     -ac 1 -c:v mjpeg -map 0:a -map 1:v \
-    "${output}"
-
-rm -f extract_tmp*jpg
+    "$(basename "${INPUT}").mp3"
