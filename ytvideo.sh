66a5e69df3ccbf6a4dfcfb2cc2e01864ce714659
azimut <azimut.github@protonmail.com>
2025-01-31 23:51:43 -0300

    ytvideo.sh: fix it

--- ytvideo.sh
+++ ytvideo.sh
@@ -5,13 +5,16 @@ usage() {
     echo -e "youtube-dl wrapper, with a fzf menu for selecting the url audio/video format combo.\n"
     echo -e "Usage:\n\t$(basename "$0") URL"
 }
-(($# != 2)) && usage && exit
+(($# != 1)) && usage && exit
 
-formats="$(bkt --ttl 1h -- yt-dlp -F "$1" |
-    grep '^[0-9]' |
-    fzf -m2 |
-    awk '{ print $1 } END { if (NR!=2) exit 1 }' |
-    paste -sd+)"
+formats="$(
+    bkt --ttl 1h -- yt-dlp -F "$1" |
+        grep '^[0-9]' |
+        grep --color=always -e '^' -e 'video only' |
+        fzf -m2 --ansi |
+        awk '{ print $1 }' |
+        paste -sd+
+)"
 
 set +x
 mpv --ytdl-format="${formats}" "$1"
