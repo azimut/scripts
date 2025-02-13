344e1dc71ba59a43ea00caefc48e991ab4608059
azimut <azimut.github@protonmail.com>
2025-02-13 07:27:32 -0300

    ffmpegfilters.sh: init

---
+++ ffmpegfilters.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -euo pipefail
+
+while true; do
+    filter="$(
+        ffmpeg -loglevel quiet -filters |
+            tail +9 |
+            fzf |
+            awk '{ print $2 }'
+    )"
+    ffmpeg -help filter="${filter}" 2>/dev/null |
+        sed "1s/Filter/\t/;s/^${filter} AVOptions://" | less
+done
