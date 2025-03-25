ec992115f92ff04abb70744f1d71ba14da5fc9f6
azimut <azimut.github@protonmail.com>
2025-03-25 14:55:38 -0300

    radios: exit on error
    since it's a big chain of pipes all run at the same time
     including the fzf, so it just doesnt abort

--- radios.sh
+++ radios.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
-set -eo pipefail
+set -exo pipefail
 
 URL="http://all.api.radio-browser.info/json/stations/bycountrycodeexact/${1:-ar}"
+
 while :; do
     bkt --discard-failures --ttl=24hour -- curl --fail -s "${URL}" |
-        jq -r 'def trim: gsub("^[ ]+|[ ]+$";"");
-               map(select(.lastcheckok > 0)) |
-               unique_by(.url_resolved) |
-               unique_by(.name|ascii_downcase|trim)[] |
-               [(.name|trim),.url_resolved] |
-               @tsv' |
-        sort |
-        fzf --with-nth=1 --delimiter=$'\t' |
-        cut -f2 |
-        xargs --no-run-if-empty mpv
+        while read -r radiourl; do
+            echo "${radiourl}" |
+                jq -r 'def trim: gsub("^[ ]+|[ ]+$";"");
+                       map(select(.lastcheckok > 0)) |
+                       unique_by(.url_resolved) |
+                       unique_by(.name|ascii_downcase|trim)[] |
+                       [(.name|trim),.url_resolved] |
+                       @tsv' |
+                sort |
+                fzf --with-nth=1 --delimiter=$'\t' |
+                cut -f2 |
+                xargs --no-run-if-empty mpv
+        done
 done
