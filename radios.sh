e4779b1a3329552de0b5949c97029ea6c73b8529
azimut <azimut.github@protonmail.com>
2025-03-25 15:29:58 -0300

    radios: fix back, hopefully still crashes on error

--- radios.sh
+++ radios.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
-set -exo pipefail
+set -euo pipefail
 
 URL="http://all.api.radio-browser.info/json/stations/bycountrycodeexact/${1:-ar}"
+radios="$(bkt --discard-failures --ttl=24hour -- curl --fail -s "${URL}")"
 
 while :; do
-    bkt --discard-failures --ttl=24hour -- curl --fail -s "${URL}" |
-        while read -r radiourl; do
-            echo "${radiourl}" |
-                jq -r 'def trim: gsub("^[ ]+|[ ]+$";"");
-                       map(select(.lastcheckok > 0)) |
-                       unique_by(.url_resolved) |
-                       unique_by(.name|ascii_downcase|trim)[] |
-                       [(.name|trim),.url_resolved] |
-                       @tsv' |
-                sort |
-                fzf --with-nth=1 --delimiter=$'\t' |
-                cut -f2 |
-                xargs --no-run-if-empty mpv
-        done
+    jq -r 'def trim: gsub("^[ ]+|[ ]+$";"");
+           map(select(.lastcheckok > 0)) |
+           unique_by(.url_resolved) |
+           unique_by(.name|ascii_downcase|trim)[] |
+           [(.name|trim),.url_resolved] |
+           @tsv' <<<"${radios}" |
+        sort |
+        fzf --with-nth=1 --delimiter=$'\t' |
+        cut -f2 |
+        xargs --no-run-if-empty mpv
 done
