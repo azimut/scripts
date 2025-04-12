d1a970fbb9aa5c90dbb276f6c384f3e96ce7c3a6
azimut <azimut.github@protonmail.com>
2025-04-11 21:14:24 -0300

    urllinks add usage

--- urllinks.sh
+++ urllinks.sh
@@ -1,5 +1,13 @@
-#!/bin/sh
+#!/bin/bash
+set -eo pipefail
+
 export LC_ALL=C # byte sort comparison
+usage() {
+    echo "Lists unique <a>nchor links on the given url."
+    echo -e "Usage:\n\t$(basename "$0") URL"
+}
+(($# != 1)) && usage && exit 1
+
 bkt --ttl=1hour -- \
     lynx -dump -listonly -nonumbers -image_links -hiddenlinks=merge "$1" |
     sort -u
