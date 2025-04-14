5837a820c2099060b48ba53d4388b8321e8cbc0d
azimut <azimut.github@protonmail.com>
2025-04-14 15:27:30 -0300

    resolutions: init

---
+++ resolutions.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo -n "16:9 -"
+for ((i = 10; i < 150; i = i + 10)); do
+    printf " %dx%d" $((16 * i)) $((9 * i))
+done
+echo
+echo -n "4:3 -"
+for ((i = 40; i < 400; i = i + 20)); do
+    printf " %dx%d" $((4 * i)) $((3 * i))
+done
+echo
