0058ba8a9382b8e4d85d8a07c92aa31ac877441a
azimut <azimut.github@protonmail.com>
2025-07-24 18:22:13 -0300

    codeimage: refactor

--- codeimage.sh
+++ codeimage.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
-set -exuo pipefail
+set -xuo pipefail
 
 CODE="${1}"
 BACK="${2}"
 
 freeze \
     --line-height 1.4 \
-    --show-line-numbers \
-    --font.size 10 \
-    --background "#ff0000" \
+    --font.size 11 \
     --border.color "#515151" --border.radius 8 --border.width 2 \
     --font.family LiberationMono \
     -t github-dark \
-    -o out.png \
+    -o code.png \
     "${CODE}"
 
-code_dimensions=($(identify -format "%w %h" out.png))
-back_dimensions=($(identify -format "%w %h" "${BACK}"))
+read -r -a code_dimensions < <(identify -format "%w %h" code.png)
+read -r -a back_dimensions < <(identify -format "%w %h" "${BACK}")
 
 echo "${code_dimensions[0]}"
 echo "${back_dimensions[0]}"
 
 new_width="$(echo 0k "${code_dimensions[0]}" 1.4 / p | dc)"
 convert "${BACK}" \
-    \( out.png -resize "${new_width}"x "${BACK}" \) \
+    \( code.png -resize "${new_width}"x "${BACK}" \) \
     -gravity Center -composite out3.png
