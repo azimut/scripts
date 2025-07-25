61bd3427f067304400a02df66fd8ddc9285dd8b7
azimut <azimut.github@protonmail.com>
2025-07-25 05:01:17 -0300

    add shadow and "proper" compose

--- codeimage.sh
+++ codeimage.sh
@@ -1,18 +1,31 @@
 #!/bin/bash
-set -xuo pipefail
+set -exuo pipefail
 
 CODE="${1}"
 BACK="${2}"
+OUTPUT=out.png
+
+rm -vf "${OUTPUT}"
 
 freeze \
     --line-height 1.4 \
     --font.size 11 \
-    --border.color "#515151" --border.radius 8 --border.width 2 \
+    --border.color "#515151" --border.radius 8 --border.width 4 \
     --font.family LiberationMono \
     -t github-dark \
     -o code.png \
     "${CODE}"
 
-convert "${BACK}" \
-    \( code.png -resize 70%x "${BACK}" \) \
-    -gravity Center -composite out3.png
+convert \
+    \( code.png -resize 70%x \) \
+    \( +clone -background black -shadow 80x20+20+0 \) \
+    +swap \
+    -background none \
+    -layers merge \
+    +repage \
+    "${BACK}" \
+    +swap \
+    -gravity Center \
+    -compose Multiply \
+    -composite \
+    "${OUTPUT}"
