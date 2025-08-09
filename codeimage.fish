06068f9f59ed1fc5b76e05fcb85d2b4cb9024d08
azimut <azimut.github@protonmail.com>
2025-08-09 01:28:03 -0300

    codeimag: add github theme arg

--- codeimage.fish
+++ codeimage.fish
@@ -8,15 +8,18 @@ function usage
     echo "$script - Overlays an image of source code over the given image."
     echo "Usage:  $script [arguments] CODE IMAGE" >&2
     echo
-    echo "  -a/--alpha - Code alpha transparency %."
-    echo "  -s/--scale - Code scale."
-    echo "  -f/--font  - Font name."
+    echo "  -a/--alpha - Code alpha transparency. Default: 0.6"
+    echo "  -s/--scale - Code scale.              Default: 20"
+    echo "  -f/--font  - Code font name.          Default: LiberationMono"
+    echo "  -t/--theme - Code color theme.        Default: github-dark"
+    echo
 end
 
-argparse -n $script -N2 -X2 's/scale=!_validate_int' 'a/alpha=' 'f/font=' -- $argv || usage && exit 1
+argparse -N2 -X2 's/scale=!_validate_int' 'a/alpha=' 'f/font=' 't/theme=' -- $argv || usage && exit 1
 set -q _flag_scale || set _flag_scale 20
 set -q _flag_alpha || set _flag_alpha 0.6
-set -q _flag_font  || set _flag_font 'LiberationMono'
+set -q _flag_font  || set _flag_font LiberationMono
+set -q _flag_theme || set _flag_theme github-dark
 
 set CODE $argv[1]
 set BACK $argv[2]
@@ -26,7 +29,7 @@ freeze \
     --font.size 11 \
     --border.color "#515151" --border.radius 8 --border.width 4 \
     --font.family $_flag_font \
-    -t github-dark \
+    -t $_flag_theme \
     -o code.png \
     $CODE
 
