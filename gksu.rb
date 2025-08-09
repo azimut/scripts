cca0a93799b4893352e1039410caae84d3f70365
azimut <azimut.github@protonmail.com>
2025-08-09 01:09:10 -0300

    gksu: accept dirs as argument, fix

--- gksu.rb
+++ gksu.rb
@@ -6,14 +6,15 @@ abort('Invalid number of arguments.') if ARGV.length <= 2
 abort('File does not exits.') unless File.exist?(ARGV[0])
 
 path = File.expand_path(ARGV[0])
+safepath = path.shellescape
 cmd = ARGV[1..].map(&:shellescape).join(' ')
 
 puts "[*] Running #{cmd}"
 if File.owned? path
-  system("#{cmd}", exception: true)
+  system("#{cmd} #{safepath}", exception: true)
 elsif system('sudo -n true')
-  system("sudo #{cmd}", exception: true)
+  system("sudo #{cmd} #{safepath}", exception: true)
 else
-  system("zenity --password | sudo -S #{cmd}", exception: true)
+  system("zenity --password | sudo -S #{cmd} #{safepath}", exception: true)
 end
 puts '[*] Done!'
