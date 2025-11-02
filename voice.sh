21fcee99556417a8d0b655acd2a72a3c78ddf8fc
azimut <azimut.github@protonmail.com>
2025-11-02 13:56:50 -0300

    format voice.sh

--- voice.sh
+++ voice.sh
@@ -2,6 +2,10 @@
 
 trap 'kill "${VOICE_PID}"' EXIT
 
-coproc VOICE { while read -r phrase; do spd-say --wait -l es "${phrase}"; done; }
+coproc VOICE {
+    while read -r phrase; do
+        spd-say --wait -l es "${phrase}"
+    done
+}
 
 rlwrap cat >&"${VOICE[1]}"
