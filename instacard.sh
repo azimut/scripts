81ae0432252952bbf987098100c7fa0911c17ead
azimut <azimut.github@protonmail.com>
2026-04-10 18:46:25 -0300

    instacard: add yad colors

--- instacard.sh
+++ instacard.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -xu
+set -xue
 
 FONT='Spotify-Mix-Regular' # see ~/.local/share/fonts/*.ttf
 
@@ -10,8 +10,11 @@ yad --form --escape-ok --title="magick thumbnail" \
     --field="Author" \
     --field="Title" \
     --field="Thumbnail:":FL \
-    --field="Background:":CHK |
-    while IFS='|' read -r AUTHOR TITLE COVER BACKGROUND; do
+    --field="Background:":CHK \
+    --field="Title:":CLR \
+    --field="Author:":CLR \
+    '' '' '' FALSE '#ffffff' '#808080' |
+    while IFS='|' read -r AUTHOR TITLE COVER BACKGROUND TCOLOR ACOLOR; do
         # Square it + Bevel
         magick \
             "${COVER}" \
@@ -35,8 +38,8 @@ yad --form --escape-ok --title="magick thumbnail" \
                 -size 660x880 xc:none \
                 -draw "fill rgba(0,0,0,0.65) roundrectangle 0,0 %[fx:w],%[fx:h] 40,40" \
                 -font "${FONT}" \
-                -draw "fill white font-size 60 text %[fx:w*0.08],%[fx:h*0.83] '${TITLE}'" \
-                -draw "fill gray  font-size 50 text %[fx:w*0.08],%[fx:h*0.89] '${AUTHOR}'" \
+                -draw "fill ${TCOLOR} font-size 60 text %[fx:w*0.08],%[fx:h*0.83] '${TITLE}'" \
+                -draw "fill ${ACOLOR} font-size 50 text %[fx:w*0.08],%[fx:h*0.89] '${AUTHOR}'" \
                 "${0##*/}.cover.png" \
                 -gravity north -geometry '+0+%[fx:v.h*0.1]' -composite \
                 "${0##*/}.png"
@@ -45,8 +48,8 @@ yad --form --escape-ok --title="magick thumbnail" \
                 "${0##*/}.cover.png" \
                 -gravity South -background transparent -splice 0x100 \
                 -font "${FONT}" \
-                -pointsize 40 -fill white -annotate +0+25 "${TITLE}" \
-                -pointsize 30 -fill gray -annotate +0+0 "${AUTHOR}" \
+                -pointsize 40 -fill "${TCOLOR}" -annotate +0+25 "${TITLE}" \
+                -pointsize 30 -fill "${ACOLOR}" -annotate +0+0 "${AUTHOR}" \
                 "${0##*/}.png"
         fi
 
